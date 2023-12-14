22bdcb9e0befb9c71013df9783fbf186f5303358
NikoAlvinSimanjuntak17 <124249309+NikoAlvinSimanjuntak17@users.noreply.github.com>
2023-12-14 16:43:22 +0700

    Code

--- request_app/lib/Mahasiswa.views/RequestIzinBermalam.dart
+++ request_app/lib/Mahasiswa.views/RequestIzinBermalam.dart
@@ -27,7 +27,7 @@ class _RequestIzinBermalamScreenState extends State<RequestIzinBermalamScreen> {
       if (response.error == null) {
         setState(() {
           _izinbermalamlist = response.data as List<dynamic>;
-          _loading = _loading ? !_loading : _loading;
+          _loading = false;
         });
       } else if (response.error == unauthrorized) {
         logout().then((value) => {
@@ -58,9 +58,9 @@ class _RequestIzinBermalamScreenState extends State<RequestIzinBermalamScreen> {
       ApiResponse response = await DeleteIzinBermalam(id);
 
       if (response.error == null) {
-        await Future.delayed(Duration(milliseconds: 300)); // Add this line
-        Navigator.pop(context); // Close the confirmation dialog
-        retrievePosts(); // Move retrievePosts after the dialog is closed
+        await Future.delayed(Duration(milliseconds: 300));
+        Navigator.pop(context);
+        retrievePosts();
       } else if (response.error == unauthrorized) {
         // ... (unchanged)
       } else {
@@ -75,134 +75,154 @@ class _RequestIzinBermalamScreenState extends State<RequestIzinBermalamScreen> {
 
   @override
   void initState() {
-    // Call retrievePosts to fetch data when the screen is initialized
     retrievePosts();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.blue,
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            secondary: Colors.blueAccent,
+          ) // Add more styling as needed
+          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Izin Bermalam Requests'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: _navigateToAddData,
+            ),
+          ],
+        ),
         body: _loading
-            ? Center(child: CircularProgressIndicator())
+            ? Center(
+                child: CircularProgressIndicator(
+                  color: Colors.blue,
+                ),
+              )
             : SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: DataTable(
+                  headingTextStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                  dataRowHeight: 56,
                   columns: [
                     DataColumn(label: Text('No')),
                     DataColumn(label: Text('Keperluan IB')),
                     DataColumn(label: Text('Status')),
                     DataColumn(label: Text('Actions')),
                   ],
-                  rows: _izinbermalamlist
-                      .map(
-                        (requestIzinBermalam) => DataRow(
-                          cells: [
-                            DataCell(Text(
-                                '${_izinbermalamlist.indexOf(requestIzinBermalam) + 1}')),
-                            DataCell(Text(requestIzinBermalam.reason)),
-                            DataCell(Text(requestIzinBermalam
-                                .status)), // Replace with actual status property
-                            DataCell(
-                              PopupMenuButton(
-                                itemBuilder: (BuildContext context) {
-                                  return [
-                                    PopupMenuItem(
-                                      child: Text('Edit'),
-                                      value: 'edit',
-                                    ),
-                                    PopupMenuItem(
-                                      child: Text('View'),
-                                      value: 'view',
-                                    ),
-                                    PopupMenuItem(
-                                      child: Text('Cancel'),
-                                      value: 'delete',
-                                    ),
-                                  ];
-                                },
-                                onSelected: (String value) {
-                                  if (value == 'edit') {
-                                    int index = _izinbermalamlist
-                                        .indexOf(requestIzinBermalam);
-                                    RequestIzinBermalam selectedIzinBermalam =
-                                        _izinbermalamlist[index];
+                  rows: _izinbermalamlist.map((requestIzinBermalam) {
+                    int index = _izinbermalamlist.indexOf(requestIzinBermalam);
+                    return DataRow(
+                      cells: [
+                        DataCell(Text('${index + 1}')),
+                        DataCell(Text(requestIzinBermalam.reason)),
+                        DataCell(Text(requestIzinBermalam.status)),
+                        DataCell(
+                          PopupMenuButton(
+                            itemBuilder: (BuildContext context) {
+                              return [
+                                PopupMenuItem(
+                                  child: Text('Edit'),
+                                  value: 'edit',
+                                ),
+                                PopupMenuItem(
+                                  child: Text('View'),
+                                  value: 'view',
+                                ),
+                                PopupMenuItem(
+                                  child: Text('Delete'),
+                                  value: 'delete',
+                                ),
+                              ];
+                            },
+                            onSelected: (String value) {
+                              if (value == 'edit') {
+                                int index = _izinbermalamlist
+                                    .indexOf(requestIzinBermalam);
+                                RequestIzinBermalam selectedIzinBermalam =
+                                    _izinbermalamlist[index];
 
-                                    Navigator.of(context)
-                                        .push(MaterialPageRoute(
-                                      builder: (context) => FormIzinBermalams(
-                                        title: "Edit Izin Bermalam",
-                                        formIzinBermalam: selectedIzinBermalam,
-                                      ),
-                                    ));
-                                  } else if (value == 'view') {
-                                    showDialog(
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        return AlertDialog(
-                                          title: Text("View Izin Bermalam"),
-                                          content: Text(
-                                              "Keperluan IB: ${requestIzinBermalam.reason}"),
-                                          actions: [
-                                            TextButton(
-                                              onPressed: () {
-                                                Navigator.pop(context);
-                                              },
-                                              child: Text('Close'),
-                                            ),
-                                          ],
-                                        );
-                                      },
+                                Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => FormIzinBermalams(
+                                    title: "Edit Izin Bermalam",
+                                    formIzinBermalam: selectedIzinBermalam,
+                                  ),
+                                ));
+                              } else if (value == 'view') {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Text("View Izin Bermalam"),
+                                      content: Text(
+                                          "Keperluan IB: ${requestIzinBermalam.reason}"),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: Text('Close'),
+                                        ),
+                                      ],
                                     );
-                                  } else if (value == 'delete') {
-                                    int index = _izinbermalamlist
-                                        .indexOf(requestIzinBermalam);
-                                    RequestIzinBermalam selectedIzinBermalam =
-                                        _izinbermalamlist[index];
-                                    showDialog(
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        return AlertDialog(
-                                          title: Text("Delete Izin Bermalam"),
-                                          content: Text(
-                                              "Apakah Anda yakin ingin menghapus izin bermalam ini?"),
-                                          actions: [
-                                            TextButton(
-                                              onPressed: () {
-                                                Navigator.pop(
-                                                    context); // Tutup dialog konfirmasi
-                                              },
-                                              child: Text('Batal'),
-                                            ),
-                                            TextButton(
-                                              onPressed: () {
-                                                deleteIzinBermalam(
-                                                    selectedIzinBermalam.id ??
-                                                        0);
-                                              },
-                                              child: Text('Hapus'),
-                                            ),
-                                          ],
-                                        );
-                                      },
+                                  },
+                                );
+                              } else if (value == 'delete') {
+                                int index = _izinbermalamlist
+                                    .indexOf(requestIzinBermalam);
+                                RequestIzinBermalam selectedIzinBermalam =
+                                    _izinbermalamlist[index];
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Text("Delete Izin Bermalam"),
+                                      content: Text(
+                                          "Are you sure you want to delete this request?"),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: Text('Cancel'),
+                                        ),
+                                        TextButton(
+                                          onPressed: () {
+                                            deleteIzinBermalam(
+                                                selectedIzinBermalam.id ?? 0);
+                                          },
+                                          child: Text('Delete'),
+                                        ),
+                                      ],
                                     );
-                                  }
-                                },
-                              ),
-                            ),
-                          ],
+                                  },
+                                );
+                              }
+                            },
+                          ),
                         ),
-                      )
-                      .toList(),
+                      ],
+                    );
+                  }).toList(),
                 ),
               ),
         floatingActionButton: FloatingActionButton.extended(
           onPressed: _navigateToAddData,
           label: Text('Request Here'),
           icon: Icon(Icons.add),
+          backgroundColor: Colors.blue,
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         bottomNavigationBar: BottomAppBar(
+          color: Colors.blue,
           shape: const CircularNotchedRectangle(),
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
@@ -217,13 +237,15 @@ class _RequestIzinBermalamScreenState extends State<RequestIzinBermalamScreen> {
                     );
                   },
                   child: Text(
-                    '<-  Back to Home',
-                    style: TextStyle(color: Colors.blue),
+                    'Back to Home',
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
               ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
